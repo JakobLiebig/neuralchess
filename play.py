daca64405b23472ab83061f7ff99befeeb265eca
JakobLiebig <jakobliebig@liebig.fritz.box>
2024-06-07 13:09:03 +0200

    better ui

--- play.py
+++ play.py
@@ -7,38 +7,52 @@ import treesearch
 import value_fn
 import value_net
 
-nn_path = f'{os.getcwd()}/nns/v1.0.0.nn'
-nn = value_net.Net()
-nn.load_state_dict(torch.load(nn_path))
+def get_move_dialog(legal_moves):
+    while True:
+        move_str = input('>')
 
-val_fn = value_fn.Func(nn, weight=1.8)
+        try:
+            move = chess.Move.from_uci(move_str)
 
-tree = treesearch.TreeSearch(val_fn)
-
-game = env.ChessEnv()
-
-search_depth = 3
-
-while not game.is_over():
-    if game.board.turn:
-        print(game.board)
+            if move not in legal_moves:
+                print('Please enter a legal move!')
+            else:
+                return move
+        except chess.InvalidMoveError:
+            print('Please enter a valid UCI move!')
         
-        while True:
-            move_str = input('input move (UCI)>')
+def main():
+    nn_path = f'{os.getcwd()}/nns/v1.0.0.nn'
+    nn = value_net.Net()
+    nn.load_state_dict(torch.load(nn_path))
 
-            try:
-                move = chess.Move.from_uci(move_str)
+    val_fn = value_fn.Func(nn, weight=1.8)
 
-                if move not in game.board.legal_moves:
-                    print('Please enter a legal move!')
-                else:
-                    break
-            except chess.InvalidMoveError:
-                print('Please enter a valid uci move!')
+    tree = treesearch.TreeSearch(val_fn)
+
+    game = env.ChessEnv()
+
+    search_depth = 3
+
+    print('Welcome to neural-chess!')
+    print('')
+    print('Please Note: Enter moves using the UCI format')
+    print('')
+
+    while not game.is_over():
+        if game.board.turn:
+            print(game.board)
             
-        game.push(move)
-    else:
-        _, move = tree.alphabeta(game, search_depth, False)
-        
-        print(f'Neuralchess plays {move.uci()}')
-        game.push(move)
+            move = get_move_dialog(game.board.legal_moves)
+            game.push(move)
+        else:
+            _, move = tree.alphabeta(game, search_depth, False)
+            game.push(move)
+
+            print(f'Neuralchess plays {move.uci()}')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Bye bye')
